caa5c80784927f1152d5fd888bc979866d0d74a1
Victor Cardona <viccardo@cobogpgp7878.global.publicisgroupe.net>
2018-03-12 12:06:32 -0500

    Spring-Recipe App: SpringGuruFramework course

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,24 @@
+package guru.springframework.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Notes() {
+    }
+}
